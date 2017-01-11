482a97931e9f0e35d7c92344140cc61318b8e7b2
awsm-dsgn <timurs.pleiko.iziks@gmail.com>
2017-01-11 17:08:37 +0200

    my_news moved to separate file

--- src/App.js
+++ src/App.js
@@ -2,27 +2,9 @@ import './App.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import EventEmitter from 'events'
+import {my_news} from './my_news'
 
 'use strict';
-
-// News data
-var my_news = [
-	{
-		author: 'Саша Печкин',
-		text: 'В четчерг, четвертого числа...',
-		bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
-	},
-	{
-		author: 'Просто Вася',
-		text: 'Считаю, что $ должен стоить 35 рублей!',
-		bigText: 'А евро 42!'
-	},
-	{
-		author: 'Гость',
-		text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
-		bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
-	}
-];
 
 window.ee = new EventEmitter();
 
