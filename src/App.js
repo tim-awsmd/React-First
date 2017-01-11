e641861a379381fdc296633d8f0defdbb81c1e61
awsm-dsgn <timurs.pleiko.iziks@gmail.com>
2017-01-11 18:40:45 +0200

    Article moved to separate file

--- src/App.js
+++ src/App.js
@@ -3,49 +3,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import EventEmitter from 'events'
 import news from './my_news'
+import Article from './article'
 
 
 'use strict';
 
 window.ee = new EventEmitter();
-
-var Article = React.createClass({
-	propTypes: {
-		data: React.PropTypes.shape({
-			author: React.PropTypes.string.isRequired,
-			text: React.PropTypes.string.isRequired,
-			bigText: React.PropTypes.string.isRequired
-		})
-	},
-	getInitialState: function() {
-		return {
-			visible: false
-		};
-	},
-	readmoreClick: function(e) {
-		e.preventDefault();
-		this.setState({visible: true});
-	},
-	render: function() {
-		var author = this.props.data.author,
-			text = this.props.data.text,
-			bigText = this.props.data.bigText,
-			visible = this.state.visible;
-
-		return (
-			<div className="article">
-				<p className="news__author">{author}:</p>
-				<p className="news__text">{text}</p>
-				<a href="#"
-					onClick={this.readmoreClick}
-					className={'news__readmore ' + (visible ? 'none': '')}>
-					Подробнее
-				</a>
-				<p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
-			</div>
-		)
-	}
-});
 
 // News block
 var News = React.createClass({
---
+++ src/article.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+
+var Article = React.createClass({
+	propTypes: {
+		data: React.PropTypes.shape({
+			author: React.PropTypes.string.isRequired,
+			text: React.PropTypes.string.isRequired,
+			bigText: React.PropTypes.string.isRequired
+		})
+	},
+	getInitialState: function() {
+		return {
+			visible: false
+		};
+	},
+	readmoreClick: function(e) {
+		e.preventDefault();
+		this.setState({visible: true});
+	},
+	render: function() {
+		var author = this.props.data.author,
+			text = this.props.data.text,
+			bigText = this.props.data.bigText,
+			visible = this.state.visible;
+
+		return (
+			<div className="article">
+				<p className="news__author">{author}:</p>
+				<p className="news__text">{text}</p>
+				<a href="#"
+					onClick={this.readmoreClick}
+					className={'news__readmore ' + (visible ? 'none': '')}>
+					Подробнее
+				</a>
+				<p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
+			</div>
+		)
+	}
+});
+
+export default Article
