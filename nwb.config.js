cc62ead550c06b9e5df6d9cbbdbf7d2bd0e7682f
awsm-dsgn <timurs.pleiko.iziks@gmail.com>
2017-01-11 13:28:43 +0200

    Update

--- nwb.config.js
+++ nwb.config.js
@@ -1,3 +1,35 @@
+const path = require('path');
+
 module.exports = {
-  type: 'react-app'
-}
+  type: 'react-app',
+  babel: {
+    stage: 0
+  },
+  webpack: {
+    loaders: {
+      jpeg: {
+        name: 'assets/[name].[hash:8].jpg'
+      },
+      fonts: {
+        name: 'fonts/[name].[hash:8].[ext]'
+      },
+      eot: {
+        name: 'fonts/[name].[hash:8].[ext]'
+      }
+    },
+
+    extra: {
+      module: {
+        preLoaders: [
+          {
+            test: /\.js$/,
+            loaders: ['eslint'],
+            include: [
+              path.resolve(__dirname, "src"),
+            ],
+          }
+        ],
+      }
+    }
+  }
+};
